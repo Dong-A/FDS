ae5f461ab0ffb654dd340d44ec9306af8e71cef2
동아 <Dong-A@Dong-Aui-MacBook-Pro.local>
2016-10-05 16:07:12 +0900

    none

---
+++ core.js
@@ -0,0 +1,35 @@
+
+9 + '';
+
+var n = 102;
+
+n + '';
+
+console.log(n);
+
+
+var k = 90120;
+console.log('typeof k: ', typeof k);
+
+k = String(k);
+console.log('typeof k: ', typeof k);
+
+
+var m = 9090;
+m = m.toString();
+console.log('m: ', m);
+console.log('typeof m: ', typeof m );
+
+var u = "3453";
+
+u - 0;
+u * 1;
+u / 1;
+
+var font_size = "21.9px";
+
+var parse_int = window.parseInt(font_size, 10);
+var parse_float = window.parseFloat(font_size, 10);
+
+console.log('parse_int : ', parse_int);
+console.log('parse_float : ', parse_float);
