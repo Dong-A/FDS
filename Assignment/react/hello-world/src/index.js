30af2103ae381102a00e5e9519df112f58a2c720
동아 <dongakim90@gmail.com>
2016-11-09 21:55:59 +0900

    react study

--- Assignment/react/hello-world/src/index.js
+++ Assignment/react/hello-world/src/index.js
@@ -7,3 +7,40 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+var TextAreaCounter = React.createClass({
+  propTypes: {
+    defaultValue: React.PropTypes.string,
+  },
+  getDefaultProps: function(){
+    return {
+      text: '',
+    };
+  },
+  render: function(){
+    return React.DOM.div(null,
+    React.DOM.textarea({
+      defaultValue: this.props.text,
+      onChange: this._textChange
+    }),
+    React.DOM.h3(null, this.props.text.length)
+   );
+  },
+  getInitialState: function(){
+   return{
+     text: this.props.defaultValue
+   };
+  },
+  _textChange: function(ev){
+   this.setState({
+     text: ev.target.value
+   });
+ }
+})
+
+var myTextAreaCounter = ReactDOM.render(
+  React.createElement(TextAreaCounter, {
+    text: 'Bob',
+  }),
+  document.getElementById('root')
+)
